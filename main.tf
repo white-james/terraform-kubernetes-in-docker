87ce088a03d2aca05a818491f0d1e47a3744b4fb
white-james <james.michael.white@outlook.com>
2026-03-22 15:13:30 +0700

    Update Terraform configuration for Kubernetes cluster setup to include port mapping

--- main.tf
+++ main.tf
@@ -1,4 +1,24 @@
 # Create a cluster
 resource "kind_cluster" "default" {
-    name = "test-cluster"
+    name = "dev-cluster"
+    wait_for_ready = true
+
+    kind_config {
+      kind        = "Cluster"
+      api_version = "kind.x-k8s.io/v1alpha4"
+
+      node {
+          role = "control-plane"
+
+          extra_port_mappings {
+              container_port = 80
+              host_port      = 80
+          }
+          extra_port_mappings {
+              container_port = 443
+              host_port      = 443
+          }
+      }
+  }
 }
+
